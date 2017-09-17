5e009c740f3b0a8458fb4f42668080313208f7a7
syt123450 <421537891@qq.com>
2017-09-17 15:19:57 -0700

    first commit

---
+++ src/main/java/com/nullpointerexception/NpeApplication.java
@@ -0,0 +1,12 @@
+package com.nullpointerexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpeApplication.class, args);
+	}
+}
