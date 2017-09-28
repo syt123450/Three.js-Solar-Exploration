b955979f51bef6f8c09d3b1e5e75427af8053c06
syt123450 <421537891@qq.com>
2017-09-28 13:24:46 -0700

    flashing

---
+++ src/main/resources/static/jsDemo/flashingStar_prototype.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ss on 2017/9/28.
+ */
+
+var threeElement = document.getElementById('sceneArea');
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//set y position
+camera.position.y = 4;
+
+
+var renderer = new THREE.WebGLRenderer({canvas: threeElement, antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// var geometry = new THREE.BoxGeometry(1, 1, 1);
+// var material = new THREE.MeshBasicMaterial({
+//     map: THREE.ImageUtils.loadTexture('../images/crate.jpg')
+// });
+//
+// var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+var light = new THREE.PointLight( 0xff0000, 1, 100 );
+light.position.set( 0, 0, 0 );
+scene.add( light );
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    // var timer = Date.now() * 0.0001;
+    //
+    // //set x and z position to let camera to rotate around the cube
+    // camera.position.x = Math.cos( timer ) * 8;
+    // camera.position.z = Math.sin( timer ) * 8;
+
+    //let camera to look at camera while it is rotating around the cube
+    // camera.lookAt( scene.position );
+
+    renderer.render(scene, camera);
+};
+
+render();
