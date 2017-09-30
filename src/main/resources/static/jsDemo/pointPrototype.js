cb1ae776d7e830647ea61a1dd39c4929d6a0ce46
syt123450 <421537891@qq.com>
2017-09-30 02:12:05 -0700

    handle version problem

--- src/main/resources/static/jsDemo/pointPrototype.js
+++ src/main/resources/static/jsDemo/pointPrototype.js
@@ -2,150 +2,144 @@
  * Created by ss on 2017/9/27.
  */
 
-var renderer = new THREE.WebGLRenderer({canvas: document.getElementById('sceneArea'), antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var scene, renderer, camera, earthMesh, atmosphereMesh, point;
 
-var aspect = window.innerWidth / window.innerHeight;
-var camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
-camera.position.set(0, 0, 1.5);
-
-var scene = new THREE.Scene();
-scene.add(new THREE.AmbientLight(0xffffff));
-
-var textureLoader = new THREE.TextureLoader();
-
-var earthMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    new THREE.MeshPhongMaterial({
-            map: textureLoader.load(
-                '../images/2_no_clouds_4k.jpg'
-            ),
-            bumpScale: 0.05,
-            bumpMap: textureLoader.load(
-                '../images/earthbump1k.jpg'
-            ),
-            specular: new THREE.Color('grey'),
-            specularMap: textureLoader.load(
-                '../images/water_4k.png'
-            )
-        }
-    )
-);
-
-var atmosphereMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(0.504, 32, 32),
-    new THREE.MeshPhongMaterial({
-            map: textureLoader.load(
-                '../images/fair_clouds_4k.png'
-            ),
-            transparent: true
-        }
-    )
-);
-
-var starMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(90, 64, 64),
-    new THREE.MeshBasicMaterial({
-            map: textureLoader.load(
-                '../images/galaxy_starfield.png'
-            ),
-            side: THREE.BackSide
-        }
-    )
-);
-
-var coneMesh = new THREE.Mesh(
-    new THREE.CylinderGeometry( 0.03, 0, 0.09, 0.12, 0.03 ),
-    // new THREE.MeshBasicMaterial ({wireframe: true})
-    new THREE.MeshPhongMaterial( { color: 0x085093 } )
-);
-
-var flagMesh = new THREE.Mesh(
-    new THREE.BoxGeometry( 0.12, 0.052, 0.012 ),
-    new THREE.MeshBasicMaterial({
-            map: textureLoader.load(
-                '../images/californiaFlag.png'
-            ),
-            side: THREE.BackSide
-        }
-    )
-);
-
-//you can use my addPoint function to add a cube to earth, as we need to add a flag to the earth, I will change the function latter
-var addPoint = function(Latitude, Longitude, Radius, earthMesh, object) {
-    var phi = (90 - Latitude) * (Math.PI / 180);
-    var theta = (Longitude + 180) * (Math.PI / 180);
-
-    var pointX = -((Radius) * Math.sin(phi) * Math.cos(theta));
-    var pointY = ((Radius) * Math.cos(phi));
-    var pointZ = ((Radius) * Math.sin(phi) * Math.sin(theta));
-
-    // cube = object;
-    earthMesh.add(object);
-    object.position.x = pointX;
-    object.position.y = pointY;
-    object.position.z = pointZ;
-    object.rotation.x = phi;
-
-    return object;
-};
-
-var addFlag = function(Latitude, Longitude, Radius, earthMesh, object) {
-    var phi = (90 - Latitude) * (Math.PI / 180);
-    var theta = (Longitude + 180) * (Math.PI / 180);
-
-    var pointX = -((Radius) * Math.sin(phi) * Math.cos(theta));
-    var pointY = ((Radius) * Math.cos(phi));
-    var pointZ = ((Radius) * Math.sin(phi) * Math.sin(theta));
-
-    // cube = object;
-    earthMesh.add(object);
-    object.position.x = pointX+0.02;
-    object.position.y = pointY+0.04;
-    object.position.z = pointZ+0.13;
-    object.rotation.x = phi;
-
-    return object;
-};
-
-//base on latitude and longitude, you can call my addPoint function to add a point to the earth
+//base on latitude and longitude (San Jose), you can call my addPoint function to add a point to the earth
 var latitude = 37.3382;
-var longitude = -121.8863;
-var radius = 0.55;
-var cubeObject = addPoint(latitude, longitude, radius, earthMesh, coneMesh);
-// new addPoint(latitude, longitude, radius, earthMesh, coneMesh);
-addFlag(latitude, longitude, radius, earthMesh, flagMesh);
-// var coneObject = addPoint(latitude, longitude, radius, earthMesh);
+var longitude = 121.8863;
 
+//this is the distance point from the center of the earth, 0.5 is the radius of earth, if the point is above the surface, you can set it larger
+var radius = 0.5;
 
+scene = new THREE.Scene();
+init();
+animate();
+
+//addPoint will add a new point to earth and return the point object
+point = addPoint(latitude, longitude, radius, earthMesh);
 //this is the demo how to remove the point from the earth, in this demo, the point will be removed in 5 seconds
-// setTimeout(function () {
-//     earthMesh.remove(cubeObject);
-// }, 5000);
+setTimeout(function () {
+    earthMesh.remove(point);
+}, 5000);
 
-var aggregation = new THREE.Object3D();
-aggregation.add(earthMesh);
-aggregation.add(atmosphereMesh);
-aggregation.rotateZ(-Math.PI * 23.5 / 180);
+function init() {
 
-scene.add(aggregation);
-scene.add(starMesh);
-// scene.add(coneMesh);
-// coneMesh.translateZ(1);
+    initLight();
+    initRenderer();
+    initCamera();
+    initUniverse();
+    initEarth();
+}
 
+function initLight() {
 
+    scene.add(new THREE.AmbientLight(0xffffff));
+}
 
-var animate = function () {
+function initRenderer() {
+
+    renderer = new THREE.WebGLRenderer({canvas: document.getElementById('sceneArea'), antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+function initCamera() {
+
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
+    camera.position.set(0, 0, 1.5);
+}
+
+function initUniverse() {
+
+    var starMesh = new THREE.Mesh(
+        new THREE.SphereGeometry(90, 64, 64),
+        new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load(
+                    '../images/galaxy_starfield.png'
+                ),
+                side: THREE.BackSide
+            }
+        )
+    );
+    scene.add(starMesh);
+}
+
+function initEarth() {
+
+    earthMesh = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5, 32, 32),
+        new THREE.MeshPhongMaterial({
+                map: new THREE.TextureLoader().load(
+                    '../images/2_no_clouds_4k.jpg'
+                ),
+                bumpScale: 0.05,
+                bumpMap: new THREE.TextureLoader().load(
+                    '../images/earthbump1k.jpg'
+                ),
+                specular: new THREE.Color('grey'),
+                specularMap: new THREE.TextureLoader().load(
+                    '../images/water_4k.png'
+                )
+            }
+        )
+    );
+
+    atmosphereMesh = new THREE.Mesh(
+        new THREE.SphereGeometry(0.504, 32, 32),
+        new THREE.MeshPhongMaterial({
+                map: new THREE.TextureLoader().load(
+                    '../images/fair_clouds_4k.png'
+                ),
+                transparent: true
+            }
+        )
+    );
+
+    var aggregation = new THREE.Object3D();
+    aggregation.add(earthMesh);
+    aggregation.add(atmosphereMesh);
+    aggregation.rotateZ(-Math.PI * 23.5 / 180);
+
+    scene.add(aggregation);
+}
+
+function animate() {
 
     requestAnimationFrame(animate);
-    earthMesh.rotation.y += 0.007;
-    atmosphereMesh.rotation.y += 0.007;
 
-    coneMesh.rotation.y += 0.05
+    animateEarth();
 
     renderer.render(scene, camera);
+}
 
-};
+function animateEarth() {
 
-animate();
+    earthMesh.rotation.y += 0.001;
+    atmosphereMesh.rotation.y += 0.001;
+}
+
+//you can use my addPoint function to add a cube to earth, as we need to add a flag to the earth, I will change the function latter
+function addPoint(Latidude, Longitude, Radius, earthMesh) {
+
+    var phi = (90 - Latidude) * (Math.PI / 180);
+    var theta = (Longitude + 180) * (Math.PI / 180);
+
+    var pointX = -((Radius) * Math.sin(phi) * Math.cos(theta));
+    var pointY = ((Radius) * Math.cos(phi));
+    var pointZ = -((Radius) * Math.sin(phi) * Math.sin(theta));
+
+    var geometry = new THREE.BoxGeometry(0.01, 0.01, 0.01);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+
+    var cube = new THREE.Mesh(geometry, material);
+    cube.position.x = pointX;
+    cube.position.y = pointY;
+    cube.position.z = pointZ;
+
+    earthMesh.add(cube);
+
+    return cube;
+}
