c26d034a2f83308b2e70d1e467bdafbb2c810881
Chenhua Zhu <zchholmes@gmail.com>
2017-10-17 01:01:16 -0700

    Updates for MySQLUtils: 1) add ResultSets parsing; 2) Generates two Lists for front end

--- src/main/java/com/nullpointerexception/model/utils/MySQLUtils.java
+++ src/main/java/com/nullpointerexception/model/utils/MySQLUtils.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,8 +27,10 @@ public class MySQLUtils {
     public MySQLUtils(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
+//            conn = DriverManager
+//                    .getConnection(mySQLConfig.getURL(), mySQLConfig.getUserName(), mySQLConfig.getPassword());
             conn = DriverManager
-                    .getConnection(mySQLConfig.getURL(), mySQLConfig.getUserName(), mySQLConfig.getPassword());
+                    .getConnection("jdbc:mysql://localhost:3306/cmpe202?serverTimezone=GMT&useSSL=false", "cmpe202usr", "sesame");
         } catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -58,13 +61,12 @@ public class MySQLUtils {
         } catch (Exception e) {
 
         }
-
     }
 
     public List<FuelInfoBean> getCountryDataByYear() {
 
         if (this.conn !=null){
-            String query = "SELECT areaName, longitude, latitude, amount FROM v_totalenergy";
+            String query = "SELECT areaName, longitude, latitude, Quadrillion_BTU FROM v_totalenergy";
             try {
                 preStmt = conn.prepareStatement(query);
 
@@ -89,8 +91,83 @@ public class MySQLUtils {
         return null;
     }
 
-    public List<FuelInfoBean> transferToFuelInfoBeanList(ResultSet resultSet){
+    public List<Double> getGeoAmountData(){
+        if (this.conn !=null){
+            String query = "SELECT longitude, latitude, Quadrillion_BTU FROM v_totalenergy";
+            try {
+                preStmt = conn.prepareStatement(query);
+
+                retSet = preStmt.executeQuery();
+
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                System.err.println("SQL ERROR, Exception");
+                System.err.println(e.getMessage());
+                e.printStackTrace();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return transferGeoAmountData(this.retSet);
+
+        }
+        else {
+            System.out.println("No SQL connection");
+        }
 
         return null;
+    }
+
+    public List<FuelInfoBean> transferToFuelInfoBeanList(ResultSet resultSet){
+        ArrayList<FuelInfoBean> ret = new ArrayList<FuelInfoBean>();
+
+        String areaName = "";
+        Double longitude = 0.0;
+        Double latitude = 0.0;
+        Double amount = 0.0;
+        try {
+            while (resultSet.next()){
+                areaName = resultSet.getString("areaName");
+                longitude = resultSet.getDouble("longitude");
+                latitude = resultSet.getDouble("latitude");
+                amount = resultSet.getDouble("Quadrillion_BTU");
+//                System.out.println("Row[" + resultSet.getRow() + "]:\t" + areaName + "\t" + longitude + "\t" + latitude + "\t" + amount);
+                ret.add(new FuelInfoBean(areaName, longitude, latitude, amount));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    public List<Double> transferGeoAmountData(ResultSet resultSet){
+        ArrayList<Double> ret = new ArrayList<Double>();
+
+        Double longitude = 0.0;
+        Double latitude = 0.0;
+        Double amount = 0.0;
+        try {
+            while (resultSet.next()){
+                longitude = resultSet.getDouble("longitude");
+                latitude = resultSet.getDouble("latitude");
+                amount = resultSet.getDouble("Quadrillion_BTU");
+                System.out.println("Row[" + resultSet.getRow() + "]:\t" + longitude + "\t" + latitude + "\t" + amount);
+                ret.add(longitude);
+                ret.add(latitude);
+                ret.add(amount);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args){
+        MySQLUtils mySQLUtils = new MySQLUtils();
+        List<FuelInfoBean> myList_1 =mySQLUtils.getCountryDataByYear();
+        List<Double> myList_2 =mySQLUtils.getGeoAmountData();
+        System.out.println("");
     }
 }
