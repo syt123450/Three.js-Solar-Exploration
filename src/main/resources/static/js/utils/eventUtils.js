eb11be42bd909967e84c80b83f2f21729701a681
syt123450 <421537891@qq.com>
2017-11-03 17:56:11 -0700

    update time line

--- src/main/resources/static/js/utils/eventUtils.js
+++ src/main/resources/static/js/utils/eventUtils.js
@@ -4,4 +4,27 @@
 
 EventUtils = function() {
 
+    this.defaultOnMouseWheel = function () {
+
+        var minScale = 1.3;
+        var maxScale = 2;
+        var speed = 0.3;
+        var delta;
+
+        if ( event.wheelDelta ) {
+            delta = event.wheelDelta / 40;
+        } else if ( event.detail ) {
+            delta = - event.detail / 3;
+        }
+
+        if (delta > 0 && camera.position.z < maxScale) {
+            camera.position.z = Math.min(maxScale, camera.position.z + delta * speed);
+        }
+
+        if (delta < 0 && camera.position.z > minScale) {
+            camera.position.z = Math.max(minScale, camera.position.z + delta * speed);
+        }
+    }
+
+
 };
